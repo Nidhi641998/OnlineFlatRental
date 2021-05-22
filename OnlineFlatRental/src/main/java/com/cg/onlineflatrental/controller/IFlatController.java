1018e6b91792887a20e6bd3e8c2d6240a6bcea01
nidhi <nidhi@DESKTOP-LTSBH4F>
2021-05-22 12:47:50 +0530

    Logger first commit

--- OnlineFlatRental/src/main/java/com/cg/onlineflatrental/controller/IFlatController.java
+++ OnlineFlatRental/src/main/java/com/cg/onlineflatrental/controller/IFlatController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,50 +17,78 @@ import com.cg.onlineflatrental.exception.InvalidFlatInputException;
 import com.cg.onlineflatrental.model.Flat;
 import com.cg.onlineflatrental.service.IFlatService;
 
+
 @RestController
 @RequestMapping("/flatbooking")
 public class IFlatController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(IFlatController.class);
 	@Autowired
 	private IFlatService iflatservice;
 	
 	@GetMapping("/viewAllFlat")
 	public List<Flat> viewAllFlat()
 	{
+		logger.info("===In Get Controller===");
+		logger.info("viewAllFlat URL is opened");
+		logger.info("viewAllFlat() controller is initiated");
 		return (List<Flat>) iflatservice.viewAllFlat();
 	}
 	
 	@GetMapping("/viewAllFlat/{flatId}")
 	public ResponseEntity viewFlat(@PathVariable("flatId") Integer flatId) throws FlatNotFoundException
 	{
+		logger.info("===In Get Controller===");
+		logger.info("viewAllFlat/{flatId} URL is opened");
+		logger.info("viewFlat() controller is initiated");
 		Flat flat=iflatservice.viewFlat(flatId);
+		logger.info("viewFlat() controller has executed");
 		return new ResponseEntity(flat, HttpStatus.OK);
 	}
 
 	@PostMapping("/addFlat")
 	public Flat addFlat(@RequestBody Flat flat) throws InvalidFlatInputException
 	{
-		return iflatservice.addFlat(flat);
+		logger.info("===In Post Controller===");
+		logger.info("addFlat URL is opened");
+		logger.info("addFlat() controller is initiated");
+		Flat flat1= iflatservice.addFlat(flat);
+		logger.info("addFlat() controller has executed");
+		return flat1;
+		
 	}
 	
 	@PutMapping("/updateFlat")
 	public ResponseEntity updateFlat(@RequestBody Flat flat) throws FlatNotFoundException, InvalidFlatInputException
 	{
+		logger.info("===In Put Controller===");
+		logger.info("updateFlat URL is opened");
+		logger.info("updateFlat() controller is initiated");
 		Flat flat1= iflatservice.updateFlat(flat);
+		logger.info("updateFlat() controller has executed");
 		return new ResponseEntity(flat1, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/deleteFlat/{flatId}")
 	public  ResponseEntity deleteFlatById(@PathVariable Integer flatId) throws FlatNotFoundException
 	{
+		logger.info("===In Delete Controller===");
+		logger.info("deleteFlat/{flatId} URL is opened");
+		logger.info("deleteFlatById() controller is initiated");
 		iflatservice.deleteFlatById(flatId);
+		logger.info("deleteFlatById() controller has executed");
 		return new ResponseEntity("Flat deleted successfully", HttpStatus.OK);
 	}
 	
 	@GetMapping("/viewByCost/{cost}/{availability}")
 	public List<Flat> viewAllFlatByCost(@PathVariable Float cost, @PathVariable String availability) throws FlatNotFoundException, InvalidFlatInputException 
 	{
-		return (List<Flat>)iflatservice.findByCostAndAvailability(cost,availability);
+		logger.info("===In Delete Controller===");
+		logger.info("viewByCost/{cost}/{availability} URL is opened");
+		logger.info("viewAllFlatByCost() controller is initiated");
+		List<Flat> flatList=iflatservice.findByCostAndAvailability(cost,availability);
+		logger.info("viewAllFlatByCost() controller has executed");
+		return flatList;	
 	}
 	
 //	@ResponseBody
